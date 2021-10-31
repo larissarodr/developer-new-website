4f3dcb7a55fb30a6b9b7a86ea99fe61f54021698
Larissa Rodrigues <larissa_rodr@hotmail.com>
2021-10-31 15:58:04 -0500

    Adding instagram and linkedin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { FiSettings, FiAtSign, FiPhone } from 'react-icons/fi'
+import React from "react";
+import {
+  FiSettings,
+  FiAtSign,
+  FiPhone,
+  FiInstagram,
+  FiLinkedin,
+} from "react-icons/fi";
 
-import './header.css';
+import "./header.css";
 
-export default function Header(){
-
+export default function Header() {
   return (
     <header>
-        <div className='links'>
-          <p><FiPhone size={16} color="#fff"/> Vendas: (47) 3028-2711</p>
-          <p><FiPhone size={16} color="#fff"/> Suporte: (47) 3029-2771</p>
-          <a href='//www.developer.inf.br/suporte/'><FiSettings size={20} color="#fff"/> Suporte</a>
-          <a href='//www.developer.inf.br/webmail/'><FiAtSign size={20} color="#fff"/> Webmail</a>
-        </div>
-  </header>
+      <div className="links">
+        <a href="//www.instagram.com/erpdevelopersoftware">
+          <FiInstagram size={16} color="#fff" /> Instagram
+        </a>
+        <a href="//www.linkedin.com/company/developer-software/">
+          <FiLinkedin size={16} color="#fff" /> Linkedin
+        </a>
+        <p>
+          <FiPhone size={16} color="#fff" /> Vendas: (47) 3028-2711
+        </p>
+        <p>
+          <FiPhone size={16} color="#fff" /> Suporte: (47) 3029-2771
+        </p>
+        <a href="//www.developer.inf.br/suporte/">
+          <FiSettings size={20} color="#fff" /> Suporte
+        </a>
+        <a href="//www.developer.inf.br/webmail/">
+          <FiAtSign size={20} color="#fff" /> Webmail
+        </a>
+      </div>
+    </header>
   );
 }
