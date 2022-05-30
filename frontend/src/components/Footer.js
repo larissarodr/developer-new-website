85044834c56ac2867758c6fa48ec37cfed5b4106
Larissa Rodrigues <larissa_rodr@hotmail.com>
2022-05-30 11:18:19 -0500

    Changing for 2022

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,44 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiPhone, FiAtSign } from 'react-icons/fi'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPhone, FiAtSign } from "react-icons/fi";
 
-import './footer.css';
-import logoImg from '../assets/Logo1.png';
+import "./footer.css";
+import logoImg from "../assets/Logo1.png";
 
-export default function Footer(){
-
+export default function Footer() {
   return (
     <>
       <footer>
-        <div className="separator"/>
+        <div className="separator" />
         <div className="footerInfo">
           <div className="logo">
-            <img src={logoImg} alt="Developer Software ERP Gestão Empresarial"/>
-            
-              <p> Desde 2001, fazendo a diferença <br/>
-              para os negócios de seus clientes. </p>
+            <img
+              src={logoImg}
+              alt="Developer Software ERP Gestão Empresarial"
+            />
+
+            <p>
+              {" "}
+              Desde 2001, fazendo a diferença <br />
+              para os negócios de seus clientes.{" "}
+            </p>
           </div>
           <div className="links">
             <h3>Links</h3>
             <ul>
-              <li><Link to='/solucoes'>Soluções</Link></li>
-              <li><Link to='/empresa'>Sobre</Link></li>
-              <li><Link to='/contato'>Contato</Link></li>
-              <li><a href='//www.developer.inf.br/novidades.php/'>Novidades</a></li>
+              <li>
+                <Link to="/solucoes">Soluções</Link>
+              </li>
+              <li>
+                <Link to="/empresa">Sobre</Link>
+              </li>
+              <li>
+                <Link to="/contato">Contato</Link>
+              </li>
+              <li>
+                <a href="//www.developer.inf.br/novidades.php/">Novidades</a>
+              </li>
             </ul>
           </div>
           <div className="contact-footer">
             <h3>Contato</h3>
-            <p><FiPhone size={14} color="#000"/> Vendas: (47) 3028-2711</p>
-            <p><FiPhone size={14} color="#000"/> Suporte: (47) 3029-2771</p>
-            <p><FiAtSign size={14} color="#000"/> developer@developer.inf.br</p>
+            <p>
+              <FiPhone size={14} color="#000" /> Vendas: (47) 3028-2711
+            </p>
+            <p>
+              <FiPhone size={14} color="#000" /> Suporte: (47) 3029-2771
+            </p>
+            <p>
+              <FiAtSign size={14} color="#000" /> developer@developer.inf.br
+            </p>
             <p>Joinville, SC</p>
           </div>
         </div>
         <div className="copyright">
-          <p>2021 - Powered by Developer Software</p>
+          <p>2022 - Powered by Developer Software ©</p>
         </div>
-      </footer>    
-    </>    
+      </footer>
+    </>
   );
 }
