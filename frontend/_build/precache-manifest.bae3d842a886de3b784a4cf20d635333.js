6312e6f6dbdad46b4a7bf31db6d92067d51c0c01
unknown <elaine.souza@developer.inf.br>
2025-01-22 13:34:31 -0300

    atualizando

---
+++ frontend/_build/precache-manifest.bae3d842a886de3b784a4cf20d635333.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0ada9c160ee2c21427025efcdf69f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff0b8d07a3f75736e44d",
+    "url": "/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "a194bca350530cb35bed",
+    "url": "/static/css/main.6265a17f.chunk.css"
+  },
+  {
+    "revision": "ff0b8d07a3f75736e44d",
+    "url": "/static/js/2.c059825f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c059825f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a194bca350530cb35bed",
+    "url": "/static/js/main.02d81d3c.chunk.js"
+  },
+  {
+    "revision": "4f1bfbea4965829c933a",
+    "url": "/static/js/runtime-main.109bd943.js"
+  },
+  {
+    "revision": "8c1494aeb48e66734944bbbbdceb2ecd",
+    "url": "/static/media/Logo1.8c1494ae.png"
+  },
+  {
+    "revision": "7183300d0633cfbc7d41b5f31fe61330",
+    "url": "/static/media/background.7183300d.png"
+  },
+  {
+    "revision": "ac98b93ffa8880ef29dc84bafa524e03",
+    "url": "/static/media/contatoBanner.ac98b93f.jpg"
+  },
+  {
+    "revision": "0430e9de1199e8a95b354437283c0a6c",
+    "url": "/static/media/handsChartDarker.0430e9de.jpg"
+  },
+  {
+    "revision": "991a60902e51b322ff5c8d91740aa390",
+    "url": "/static/media/iconeBI.991a6090.png"
+  },
+  {
+    "revision": "75d8d7ee929cca58bf0ea0913516cb3a",
+    "url": "/static/media/iconeCRM.75d8d7ee.png"
+  },
+  {
+    "revision": "94f66cce1a9c38b91a9b41174897220c",
+    "url": "/static/media/iconeComercial.94f66cce.png"
+  },
+  {
+    "revision": "d8509a6dbb55bb071cf69670b8c04d46",
+    "url": "/static/media/iconeControleQualidade.d8509a6d.png"
+  },
+  {
+    "revision": "2fa70179581a98972662a79473b72f3a",
+    "url": "/static/media/iconeFinanceiro.2fa70179.png"
+  },
+  {
+    "revision": "52e45e322916604a9b64c55e535fbb38",
+    "url": "/static/media/iconeGestaoClinica.52e45e32.png"
+  },
+  {
+    "revision": "d44f421371cdfebb2f5b9d964a012709",
+    "url": "/static/media/iconeGestaoPessoas.d44f4213.png"
+  },
+  {
+    "revision": "e0788473a983080207bd28f04e58f237",
+    "url": "/static/media/iconeLogistica.e0788473.png"
+  },
+  {
+    "revision": "5f016ed731209269fe0f9c082198aa8e",
+    "url": "/static/media/iconeManutAtivos.5f016ed7.png"
+  },
+  {
+    "revision": "488bcf6e98d2a78b0bc7882912f9194d",
+    "url": "/static/media/iconeMedicina.488bcf6e.png"
+  },
+  {
+    "revision": "ea4ab9cd2315cd11119f9a53355f9c64",
+    "url": "/static/media/iconeProducao.ea4ab9cd.png"
+  },
+  {
+    "revision": "a7d65c208bf18d33294ac6fcbf867d82",
+    "url": "/static/media/iconeServicos.a7d65c20.png"
+  },
+  {
+    "revision": "c86a966f134a9824bce47125235d8873",
+    "url": "/static/media/iconeUniversal.c86a966f.png"
+  },
+  {
+    "revision": "4e4436a22cf03929af8d37e7520974b4",
+    "url": "/static/media/linhadotempo.4e4436a2.png"
+  },
+  {
+    "revision": "5e41129a5156e311feab30232ab5bbb2",
+    "url": "/static/media/solutionsBanner.5e41129a.jpg"
+  }
+]);
